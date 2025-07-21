00b97effbed31fba18bc189ac1ea142951e6f86f
azeturk810 <azeturk810nf@gmail.com>
2025-07-21 18:46:17 +0400

    egoi elave

---
+++ egoi/2022/day1/b/cur.cpp
@@ -0,0 +1,38 @@
+/*
+Telebe of Adicto && Mamedov yani AzeTurk810
+I see humans but no humanity
+*/
+#include <iostream>
+
+using ll = long long;
+using namespace std;
+
+#define ln '\n'
+#define INFi 1e9
+#define INFll 1e18
+#define MOD 1000000007
+
+struct prob {
+    void init() {
+        
+    }
+    void in() {
+        
+    }
+    void solve() {
+        
+    }
+} prob;
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int t = 1;
+//    cin >> t;
+    for(int cases = 0 ; cases < t;cases ++) {
+         prob.init();
+         prob.in();
+         prob.solve();
+    }
+}
+// Just Imaginary
